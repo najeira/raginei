fc73be1cdc998b9ad467e13ede42ce46ac73401d
najeira <najeira@gmail.com>
2011-12-28 22:37:42 +0900

    update testutil to setup paths

--- lib/raginei/testutil.py
+++ lib/raginei/testutil.py
@@ -4,17 +4,58 @@ import os
 import sys
 import unittest
 
-def setup_path(gae_home):
+
+def setup_path(DIR_PATH):
+  
+  ### from dev_appserver.py
+  
+  SCRIPT_DIR = os.path.join(DIR_PATH, 'google', 'appengine', 'tools')
+  GOOGLE_SQL_DIR = os.path.join(
+      DIR_PATH, 'google', 'storage', 'speckle', 'python', 'tool')
+  
+  EXTRA_PATHS = [
+    DIR_PATH,
+    os.path.join(DIR_PATH, 'lib', 'antlr3'),
+    os.path.join(DIR_PATH, 'lib', 'django_0_96'),
+    os.path.join(DIR_PATH, 'lib', 'fancy_urllib'),
+    os.path.join(DIR_PATH, 'lib', 'ipaddr'),
+    os.path.join(DIR_PATH, 'lib', 'protorpc'),
+    os.path.join(DIR_PATH, 'lib', 'webob'),
+    os.path.join(DIR_PATH, 'lib', 'webapp2'),
+    os.path.join(DIR_PATH, 'lib', 'yaml', 'lib'),
+    os.path.join(DIR_PATH, 'lib', 'simplejson'),
+    os.path.join(DIR_PATH, 'lib', 'google.appengine._internal.graphy'),
+  ]
+  
+  GOOGLE_SQL_EXTRA_PATHS = [
+    os.path.join(DIR_PATH, 'lib', 'enum'),
+    os.path.join(DIR_PATH, 'lib', 'google-api-python-client'),
+    os.path.join(DIR_PATH, 'lib', 'grizzled'),
+    os.path.join(DIR_PATH, 'lib', 'httplib2'),
+    os.path.join(DIR_PATH, 'lib', 'oauth2'),
+    os.path.join(DIR_PATH, 'lib', 'prettytable'),
+    os.path.join(DIR_PATH, 'lib', 'python-gflags'),
+    os.path.join(DIR_PATH, 'lib', 'sqlcmd'),
+  ]
+  
+  SCRIPT_EXCEPTIONS = {
+    "dev_appserver.py" : "dev_appserver_main.py"
+  }
+  
+  SCRIPT_DIR_EXCEPTIONS = {
+    'google_sql.py': GOOGLE_SQL_DIR,
+  }
+  
+  extra_paths = EXTRA_PATHS[:]
+  if False: #FIXME
+    extra_paths.extend(GOOGLE_SQL_EXTRA_PATHS)
+  sys.path = extra_paths + sys.path
+  
   PROJECT_HOME = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-  for v in [gae_home, os.path.join(gae_home, 'lib', 'yaml', 'lib'),
-    PROJECT_HOME, os.path.join(PROJECT_HOME, 'lib')]:
-    if v not in sys.path:
-      sys.path.insert(0, v)
-  for v in ('antlr3', 'fancy_urllib', 'graphy', 'ipaddr', 'protorpc',
-    'simplejson', 'webob'):
-    v = os.path.join(gae_home, 'lib', v)
-    if v not in sys.path:
-      sys.path.insert(0, v)
+  sys.path.insert(0, PROJECT_HOME)
+  
+  LIB_PATH = os.path.join(PROJECT_HOME, 'lib')
+  sys.path.insert(0, LIB_PATH)
 
 
 def get_base(gae_home):
@@ -58,6 +99,12 @@ def get_base(gae_home):
       self.testbed.init_mail_stub()
       self.testbed.init_taskqueue_stub()
       self.testbed.init_urlfetch_stub()
+      
+      #set mode to high replication
+      from google.appengine.datastore import datastore_stub_util
+      datastore_stub = self.testbed.get_stub(testbed.DATASTORE_SERVICE_NAME)
+      datastore_stub.SetConsistencyPolicy(
+        datastore_stub_util.TimeBasedHRConsistencyPolicy())
     
     def _env_tearDown(self):
       self.testbed.deactivate()
