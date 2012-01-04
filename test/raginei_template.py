53d994a44adee2ec8c7fb76dbacda58529ea94b9
najeira <najeira@gmail.com>
2012-01-04 18:30:44 +0900

    add tests for templating

---
+++ test/raginei_template.py
@@ -0,0 +1,36 @@
+﻿# -*- coding:utf-8 -*-
+
+import unittest
+from base import GaeTestCase
+from werkzeug.test import Client
+
+
+class MyTest(GaeTestCase):
+  
+  def setUp(self):
+    super(MyTest, self).setUp()
+  
+  def tearDown(self):
+    super(MyTest, self).tearDown()
+  
+  def init_app(self, **kwds):
+    from raginei.app import Application
+    from raginei.wrappers import Response
+    app = Application.instance(test=True, template_dir='test/templates', **kwds)
+    c = Client(app, Response)
+    return app, c
+  
+  def test_fetch(self):
+    from raginei.app import fetch, render
+    app, c = self.init_app()
+    msg = 'Hello World!'
+    @app.route('/')
+    def hello_world():
+      return fetch('test_fetch', msg=msg)
+    res = c.get('/')
+    assert res.status_code == 200, res.status_code
+    assert res.data == msg, res.data
+
+
+if __name__ == '__main__':
+    unittest.main()
