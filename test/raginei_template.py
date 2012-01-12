cf92f58b5e2416a71719a9de61b2176a431b63f1
najeira <najeira@gmail.com>
2012-01-13 00:33:15 +0900

    add tests for template

--- test/raginei_template.py
+++ test/raginei_template.py
@@ -1,6 +1,7 @@
 ﻿# -*- coding:utf-8 -*-
 
 import unittest
+import datetime
 from base import GaeTestCase
 from werkzeug.test import Client
 
@@ -26,10 +27,37 @@ class MyTest(GaeTestCase):
     msg = 'Hello World!'
     @app.route('/')
     def hello_world():
-      return fetch('test_fetch', msg=msg)
+      ret = fetch('test_fetch', msg=msg)
+      assert isinstance(ret, basestring)
+      return ret
     res = c.get('/')
     assert res.status_code == 200, res.status_code
     assert res.data == msg, res.data
+  
+  def test_render(self):
+    from raginei.app import fetch, render
+    app, c = self.init_app()
+    msg = 'Hello World!'
+    @app.route('/')
+    def hello_world():
+      ret = render('test_fetch', msg=msg)
+      assert not isinstance(ret, basestring)
+      return ret
+    res = c.get('/')
+    assert res.status_code == 200, res.status_code
+    assert res.data == msg, res.data
+  
+  def test_helper_date(self):
+    from raginei.app import fetch, render
+    now = datetime.datetime.utcnow()
+    app, c = self.init_app()
+    @app.route('/')
+    def hello_world():
+      return render('test_helper_date', now=now)
+    res = c.get('/')
+    assert res.status_code == 200, res.status_code
+    assert res.data == ('%s,%s' % (
+      now.strftime('%Y/%m/%d %H:%M'), now.strftime('%Y/%m/%d'))), res.data
 
 
 if __name__ == '__main__':
