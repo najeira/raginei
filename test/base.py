5869ed34310089ea89748ba10e35f3a5ecffcab6
najeira <najeira@gmail.com>
2012-01-19 15:42:59 +0900

    fix test

--- test/base.py
+++ test/base.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-lib_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'lib')
+lib_path = os.path.dirname(os.path.dirname(__file__))
 if lib_path not in sys.path:
   sys.path.insert(0, lib_path)
 
